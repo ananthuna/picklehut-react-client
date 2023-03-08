62e42b4ed54c4fe76764e343c3cf8e881c62d5e2
ananthu <ananthu4uuu@gmail.com>
2023-03-08 13:15:20 +0530

    build

---
+++ build/precache-manifest.a31cbca0b360ed54504f6eeea6f04ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89d120fb25bd657d64de",
+    "url": "/static/css/main.d7c5a774.chunk.css"
+  },
+  {
+    "revision": "89d120fb25bd657d64de",
+    "url": "/static/js/main.89d120fb.chunk.js"
+  },
+  {
+    "revision": "df93f121281cbbc60dcc",
+    "url": "/static/css/1.8f7cc955.chunk.css"
+  },
+  {
+    "revision": "df93f121281cbbc60dcc",
+    "url": "/static/js/1.df93f121.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba18eb8241f33035ca33f11c0d99a8cc",
+    "url": "/index.html"
+  }
+];
